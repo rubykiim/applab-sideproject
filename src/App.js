c778b63694b5b9e23f7533560e149b3c6cd6af5a
Ruby <IKID@gimseung-aui-MacBook-Pro.local>
2019-11-27 12:38:10 +0800

    setup1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [showPg, setShowPg] = useState(false);
+
+  function handleClicked() {
+    setShowPg(true);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showPg && <Home onClick={handleClicked} />}
+      {showPg && <Page />}
+    </div>
+  );
+}
+
+function Home(props) {
+  return (
+    <div className="header">
+    <h1 id="firstHeader">Cookie</h1>
+    <h1 id="secondHeader">Challenge</h1>
+    <button onClick={props.onClick}>Start</button>
+  </div>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <h1></h1>
+      <input placeholder="Enter the date" type="text"/>
     </div>
   );
 }
